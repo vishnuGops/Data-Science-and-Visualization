ab4c8964a611813c11865eb7eb09d71deffc0cef
Vishnu <vishvargops@gmail.com>
2022-04-10 01:57:27 -0700

    commit

--- Tech Salary Visualization/Tech Salary Real Time Visualization.py
+++ Tech Salary Visualization/Tech Salary Real Time Visualization.py
@@ -10,10 +10,10 @@ dev_salaries = data['All_Devs']
 py_salaries = data['Python']
 js_salaries = data['JavaScript']
 
-plt.plot(ages, py_salaries, label='Python')
-plt.plot(ages, js_salaries, label='JavaScript')
+plt.plot(ages, py_salaries, color='#ba112a', label='Python')
+plt.plot(ages, js_salaries, color='#21239c', label='JavaScript')
 
-plt.plot(ages, dev_salaries, color='#444444',
+plt.plot(ages, dev_salaries, color='#15b034',
          linestyle='--', label='All Devs')
 
 plt.legend()
